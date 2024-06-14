87ffa134c9d642a85cf303dbbb8cfd68058f011e
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2024-06-15 02:27:13 +0530

    made chnges in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sequelize = require('./config/database');
 const dotenv = require('dotenv');
 const logger = require('./utils/logger');
 const morgan = require('morgan');
+const cors = require("cors");
 
 dotenv.config();
 
@@ -15,18 +16,48 @@ const progressRoutes = require('./src/routes/progress');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
 
 
 app.use(morgan('combined', { stream: logger.stream }));
 
-app.get("/isAlive",(req,res)=>{
-    return res.status(200).json({msg:"Hey! I am alive."})
-})
+//Ignore this endpoint it is used for faster response from the server.
 
-app.get("/",(req,res)=>{
-return   res.redirect("https://documenter.getpostman.com/view/25332112/2sA3XQgMhF");
+let maxRequests = 300000;
+let interval = 300000;
+let url = "https://continouessssss.onrender.com/isAlive";
+app.get('/isAlive', (req, res) => {
+    let count = 0;
+
+    function sendRequest() {
+        if (count < maxRequests) {
+            axios.get(url)
+                .then(response => {
+                    console.log(`Request #${count + 1} successful`, response.data);
+                    res.write(`Request #${count + 1} successful: ${JSON.stringify(response.data)}\n`);
+                })
+                .catch(error => {
+                    console.error(`Request #${count + 1} failed`, error);
+                    res.write(`Request #${count + 1} failed: ${error.message}\n`);
+                })
+                .finally(() => {
+                    count++;
+                    if (count < maxRequests) {
+                        setTimeout(sendRequest, interval);
+                    } else {
+                        res.end('Completed 300,000 requests\n');
+                    }
+                });
+        }
+    }
+
+    sendRequest();
+});
+
+app.get("/", (req, res) => {
+    return res.redirect("https://documenter.getpostman.com/view/25332112/2sA3XQgMhF");
 
 })
 app.use('/auth', authRoutes);
